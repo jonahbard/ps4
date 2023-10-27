2150ee68d04ab2a75c3ed6490300d9a228c0f558
Daniel Katz <katz.daniel.03@gmail.com>
2023-10-27 15:40:18 -0400

    bacon graph files

---
+++ src/BaconGraphLib.java
@@ -0,0 +1,22 @@
+import java.util.List;
+import java.util.Set;
+
+public class BaconGraphLib {
+
+    public static <V,E> Graph<V,E> bfs(Graph<V,E> g, V source) {
+
+        return null;
+    }
+    public static <V,E> List<V> getPath(Graph<V,E> tree, V v) {
+
+        return null;
+    }
+    public static <V,E> Set<V> missingVertices(Graph<V,E> graph, Graph<V,E> subgraph) {
+
+        return null;
+    }
+    public static <V,E> double averageSeparation(Graph<V,E> tree, V root) {
+
+        return null;
+    }
+}
