88f28557d9036f825a9e881af381ca87ddcd58c3
Daniel Katz <katz.daniel.03@gmail.com>
2023-10-27 15:50:20 -0400

    stuff

--- src/BaconGraph.java
+++ src/BaconGraph.java
@@ -1,2 +1,10 @@
 public class BaconGraph {
+
+    // TODO: Does just the graph live here or also the methods to drive the game
+
+    // TODO: Create the code to instantiate the graph based on the input files
+    public BaconGraph() {
+
+    }
+
 }
--- src/BaconGraphLib.java
+++ src/BaconGraphLib.java
@@ -1,11 +1,32 @@
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class BaconGraphLib {
 
-    public static <V,E> Graph<V,E> bfs(Graph<V,E> g, V source) {
+    // TODO: implement all these methods
 
-        return null;
+    public static <V,E> Graph<V,E> bfs(Graph<V,E> g, V source) {
+        Graph<V, E> outG = new AdjacencyMapGraph<>();
+        if (!g.hasVertex(source)) return outG;
+
+        Set<V> visited = new HashSet<>();
+        Queue<V> toVisit = new LinkedList<>();
+
+        outG.insertVertex(source);
+        visited.add(source);
+        toVisit.add(source);
+
+        while (!toVisit.isEmpty()) {
+            V curV = toVisit.remove();
+            for (V v: g.outNeighbors(curV)) {
+                if (!visited.contains(v)) {
+                    toVisit.add(v);
+                    visited.add(v);
+                    outG.insertDirected(curV, v, g.getLabel(curV, v));
+                }
+            }
+        }
+
+        return outG;
     }
     public static <V,E> List<V> getPath(Graph<V,E> tree, V v) {
 
@@ -17,6 +38,6 @@ public class BaconGraphLib {
     }
     public static <V,E> double averageSeparation(Graph<V,E> tree, V root) {
 
-        return null;
+        return 0.0;
     }
 }
